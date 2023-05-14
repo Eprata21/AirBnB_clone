121d6ae56c52ab8c871cb7122bfdd0a7dafdea74
Abrsh7 <abrshn7@gmail.com>
2023-05-14 05:35:25 -0700

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,11 @@ class BaseModel:
         def __init__(self, *args, **kwargs):
                 """public instance attr"""
                 if kwargs:
-                    self.__dict__ = kwargs
-                    if "created_at" in kwargs:
-                        self.created_at = datetime.strptime(kwargs.get("created_at"), format)
-                    if "updated_at" in kwargs:
-                        self.updated_at = datetime.strptime(kwargs.get("updated_at"), format)
+                      for key, value in kwargs.items():
+                         if key in ['created_at', 'updated_at']:
+                             value = datetime.strptime(value, format)
+                         if key != "__class__":
+                              setattr(self, key, value)
                 else:
                     self.id = str(uuid.uuid4())
                     self.created_at = datetime.now()
@@ -28,7 +28,7 @@ class BaseModel:
 
         def __str__(self):
                 """String rep of the BaseModel"""
-                return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+                return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
         def save(self):
                 """updates the attr updated_at"""
@@ -37,8 +37,8 @@ class BaseModel:
 
         def to_dict(self):
                 """returns a dictionary with created_at and updated_at"""
-                my_dict = self.__dict__.copy()
-                my_dict['__class__'] = self.__class__.__name__
-                my_dict['created_at'] = my_dict['created_at'].isoformat()
-                my_dict['updated_at'] = my_dict['updated_at'].isoformat()
-                return my_dict
+                ob_dict = self.__dict__.copy()
+                ob_dict['__class__'] = self.__class__.__name__
+                ob_dict['created_at'] = self.created_at.isoformat()
+                ob_dict['updated_at'] = self.updated_at.isoformat()
+                return ob_dict
